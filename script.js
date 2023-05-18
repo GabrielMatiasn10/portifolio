5a007a760fc695f57f103c415362d4b864330f00
GabrielMatiasn10 <Gm4037003@gmail.com>
2023-05-18 14:13:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const options = {
+    strings: ['Frontend Developer' ],
+    typeSpeed: 190,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+  };
+  
+  const typed = new Typed('.multiple-text', options);
+  
